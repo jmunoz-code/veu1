4294643e5e11abc023b15b1be28c240bef515770
jaime muñoz <jaimemunozoyarzun@gmail.com>
2025-03-24 16:31:37 -0300

    subida 2

---
+++ vue-project/src/components/Sidebar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="sidebar">
+        <h2>Barra Lateral</h2>
+        <ul>
+            <li><router-link to="/Contador">Contador</router-link></li>
+            <li><router-link to="/ListaDeTareas">Lista De Tareas</router-link></li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+.sidebar {
+    cursor: pointer;
+    width: 200px;
+    background-color: #f0f0f0;
+    padding: 20px;
+}
+</style>
--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,23 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// router.js
+import { createRouter, createWebHistory } from 'vue-router';
+import Contador from '../components/Contador.vue'; // Asegurate que esta ruta sea correcta
+
+import ListaDeTareas from '@/components/ListaDeTareas.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/Contador',
+      name: 'contador',
+      component: Contador,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+        path: '/ListaDetareas',
+        name: 'ListaDeTareas',
+        component: ListaDeTareas,
+      },
   ],
-})
+});
 
-export default router
+export default router;
